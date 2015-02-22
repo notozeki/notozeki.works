a8f2ffdaa25cbb9ca53bdbce85e08d2f3926fc8a
notozeki <notozeki@gmail.com>
2015-02-22 20:41:40 +0900

    Revert "Awesome opening animation ;)"
    This reverts commit f2e7f8185cecf7f77dd0729b1a7c14a2d05516d4.

--- source/assets/js/application.js
+++ source/assets/js/application.js
@@ -1,27 +1,2 @@
 //= require "jquery"
 //= require "bootstrap-sprockets"
-
-(function () {
-  if (sessionStorage === undefined) {
-    console.warn("Your browser does not support sessionStorage. Some features will be disabled.");
-    return;
-  }
-
-  function openingAnimation() {
-    // Once the animation was played, never play again during this session.
-    if (sessionStorage.getItem('openingAnimationPlayed') == 'true') return;
-
-    var container = document.getElementsByClassName('container')[0];
-    var oldDisplayStyle = container.style.display;
-    container.style.display = 'none';
-
-    // Animation will start after the loading is completed.
-    window.onload = function () {
-      container.style.display = oldDisplayStyle;
-      container.className += ' animate';
-      sessionStorage.setItem('openingAnimationPlayed', 'true');
-    };
-  }
-
-  document.addEventListener('DOMContentLoaded', openingAnimation, false);
-})();
